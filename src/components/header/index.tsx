b120297c5ef90fab91f25b091783f310ff021650
Fa-biano <fbiano.souza@gmail.com>
2025-06-22 19:58:41 -0300

    feat: add dynamic rendering at Header according to user status session

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,19 @@
+'use client'
+
 import Link from 'next/link'
-import { FiUser, FiLogOut   } from 'react-icons/fi'
+import { FiUser, FiLogOut, FiLoader, FiLock } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function Header () {
+  const { status } = useSession()
+
+  const handleLogin = async () => {
+    await signIn()
+  }
+
+  const handleLogout = async () => {
+    await signOut()
+  }
   return (
     <header className='w-full flex items-center px-2 py-4 bg-white h-20 shadow-sm'>
       <div className='w-full flex items-center justify-between max-w-7xl mx-auto'>
@@ -11,14 +23,28 @@ export default function Header () {
         </h1>
         </Link>
 
-        <div className='flex items-baseline gap-4'>
-          <Link href={'/dashboard'}>
-            <FiUser size={26} color='#4b5563' />
-          </Link>
-          <button>
-            <FiLogOut size={26} color='#4b5563' />
+        { status === 'loading' && (
+          <button className='animate-spin'>
+            <FiLoader size={26} color='#4b5563' />
           </button>
-        </div>
+        )}
+
+        { status === 'unauthenticated' && (
+          <button onClick={handleLogin} className='hover:cursor-pointer'>
+            <FiLock size={26} color='#4b5563' />
+          </button>
+        )}
+
+        { status === 'authenticated' && (
+          <div className='flex items-baseline gap-4'>
+            <Link href={'/dashboard'}>
+              <FiUser size={26} color='#4b5563' />
+            </Link>
+            <button onClick={handleLogout} className='hover:cursor-pointer'>
+              <FiLogOut size={26} color='#4b5563' />
+            </button>
+          </div>
+        )}
       </div>
     </header>
   )

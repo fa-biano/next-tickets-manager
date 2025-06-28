f68ef9dffe08d186de7644558631cae273e5802d
Fa-biano <fbiano.souza@gmail.com>
2025-06-28 19:29:40 -0300

    feat: create dashboard/customer page

--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -1,5 +1,7 @@
 import { Container } from '@/components/container'
+import CardCustomer from '@/components/dashboard/customer/card'
 import { auth } from '@/lib/auth'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default async function Customer () {
@@ -7,10 +9,23 @@ export default async function Customer () {
   if (!session || !session?.user) redirect('/')
   return (
     <Container>
-      <main>
-        <div>
-          <h1>Meus Clientes</h1>
+      <main className='mt-9 mb-2'>
+        <div className='flex items-center justify-between'>
+          <h1 className='text-3xl font-bold'>Meus clientes</h1>
+          <Link href='/dashboard/customer/new' className='bg-blue-500 text-white px-4 py-1 rounded'>
+            Novo cliente
+          </Link>
         </div>
+
+        <section className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2'>
+          <CardCustomer />
+
+          <CardCustomer />
+
+          <CardCustomer />
+        </section>
+
+
       </main>
     </Container>
   )

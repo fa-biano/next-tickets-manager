fe36551b865ced1811e1789ff88634bb42f15119
Fa-biano <fbiano.souza@gmail.com>
2025-06-29 23:07:41 -0300

    feat: create interface ICustomer

---
+++ src/@types/customer.type.ts
@@ -0,0 +1,10 @@
+export interface ICustomer {
+  name: string
+  email: string
+  phone: string
+  id: string
+  address: string | null
+  created_at: Date | null
+  updated_at: Date | null
+  userId: string
+}

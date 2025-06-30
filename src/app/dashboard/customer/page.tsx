129202b94a89c588337290948424f41849eda76e
Fa-biano <fbiano.souza@gmail.com>
2025-06-29 23:09:44 -0300

    feat: render CardCustomer with customer from db

--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -1,12 +1,16 @@
 import { Container } from '@/components/container'
 import CardCustomer from '@/components/dashboard/customer/card'
 import { auth } from '@/lib/auth'
+import PrismaClient from '@/lib/prisma'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default async function Customer () {
   const session = await auth()
   if (!session || !session?.user) redirect('/')
+
+  const customers = await PrismaClient.customer.findMany({ where: { userId: session.user.id } })
+  
   return (
     <Container>
       <main className='mt-9 mb-2'>
@@ -18,11 +22,9 @@ export default async function Customer () {
         </div>
 
         <section className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2'>
-          <CardCustomer />
-
-          <CardCustomer />
-
-          <CardCustomer />
+          { customers.map((customer) => (
+            <CardCustomer key={customer.id} customer={customer} />
+          ))}
         </section>
 
 

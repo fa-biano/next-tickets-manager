a5ed21d9fe73f9953910befcf13b455dfbde97c8
Fa-biano <fbiano.souza@gmail.com>
2025-06-29 23:08:55 -0300

    feat: implement dynamic card info

--- src/components/dashboard/customer/card/index.tsx
+++ src/components/dashboard/customer/card/index.tsx
@@ -1,11 +1,14 @@
-export default function CardCustomer() {
+import { ICustomer } from '@/@types/customer.type'
+
+export default function CardCustomer({ customer }: { customer: ICustomer }) {
+  const { name, email, phone } = customer
   return (
     <article className='flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-105 duration-300'>
       <h2>
-        <a className='font-bold'>Nome:</a> Mercado Silva
+        <a className='font-bold'>Nome:</a> {name}
       </h2>
-      <p><a className='font-bold'>Email:</a> teste@teste.com</p>
-      <p><a className='font-bold'>Telefone:</a> XX9991019XX</p>
+      <p><a className='font-bold'>Email:</a> {email}</p>
+      <p><a className='font-bold'>Telefone:</a> {phone}</p>
 
       <button className='bg-red-500 px-4 rounded text-white mt-2 self-start'>
         Deletar
